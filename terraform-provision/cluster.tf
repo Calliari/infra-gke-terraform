b6bf19f03032e611032e5945bc90ab9722ac10ee
Calliari <jrcalliari@me.com>
2019-08-07 16:32:49 +0100

    add the gke code to deploy a basic nginx web-app

---
+++ terraform-provision/cluster.tf
@@ -0,0 +1,41 @@
+resource "google_container_cluster" "cluster" {
+  name       = "${var.cluster_name}"
+  location   = "${var.region}"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count = 2
+
+  master_auth {
+    username = "kube-user"
+    password = "gdcodnPSKswn5bcKa-pwd"
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "${var.cluster_name}"
+  location   = "${var.region}"
+  cluster    = "${google_container_cluster.cluster.name}"
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
